1b60d8e029c4b390bf5a537a6bb8964c4be9a03c
Shayanmont <shayanm@gwu.edu>
2013-02-08 17:35:05 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShopOnSHA::Application.config.secret_token = 'bb53a7653f6ad1064e63c7280fcc05bcefb5879b83d1b95543189d40be8fd1041363dafd14b5d7ac37a734817185c5d54180b6fb98b86c8c76d50ca123c71628'
