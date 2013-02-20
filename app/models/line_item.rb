f569e2e9b5bad9f2ca0a1191bc2ade0b99718f8c
Shayan Montazeri <shayanmont@gmail.com>
2013-02-19 22:01:02 -0500

    quantity feature for carts added

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,5 +1,5 @@
 class LineItem < ActiveRecord::Base
-  attr_accessible :product, :cart_id, :product_id, :id, :updated_at, :created_at
+  attr_accessible :product, :cart_id, :product_id, :id, :updated_at, :created_at, :quantity
   belongs_to :product
   belongs_to :cart
 end
