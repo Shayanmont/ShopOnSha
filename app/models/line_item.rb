83ddbd257bd5367bf674011114f2adf18bee3616
Shayan Montazeri <shayanmont@gmail.com>
2013-02-23 12:05:27 -0500

    Empty cart and total price feature added

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,4 +2,9 @@ class LineItem < ActiveRecord::Base
   attr_accessible :product, :cart_id, :product_id, :id, :updated_at, :created_at, :quantity
   belongs_to :product
   belongs_to :cart
+
+
+  def total_price
+  	product.price * quantity
+  end
 end
