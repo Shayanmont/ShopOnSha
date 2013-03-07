7f9ef0d8cf4f38cdd7987e392c25c948824d4fe2
Shayan Montazeri <shayanmont@gmail.com>
2013-03-07 17:58:24 -0500

    config file changed

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ ShopOnSHA::Application.routes.draw do
 
   resources :products do 
     get :who_bought, :on => :member
-  
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -62,11 +62,11 @@ ShopOnSHA::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => "store#index", :as => "store"
+  # root :to => "store#index", :as => "store"
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-end
+#end
